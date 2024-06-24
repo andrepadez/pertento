4ad2ea27d6f18280297ab0e56a8af3a936258664
André Alçada Padez <andrepadez@users.noreply.github.com>
2024-06-24 19:12:30 +0100

    debugging datalayer 2772

--- workspaces/servers/experiments-server/src/handlers/test-data.ts
+++ workspaces/servers/experiments-server/src/handlers/test-data.ts
@@ -6,7 +6,6 @@ export const testDataMiddleware = async (c, next) => {
   const nowValue = now.valueOf();
   const { websiteId } = c.req.query();
   const data = JSON.stringify(c.body.data);
-  console.log('testDataMiddleware', data, websiteId);
   await client.HSET(`PERTENTO:TESTDATA:${websiteId}`, nowValue, data);
 };
 
