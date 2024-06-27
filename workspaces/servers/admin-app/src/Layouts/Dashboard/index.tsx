765fd8e93a18324141c4c778a96e791123d81e0c
André Alçada Padez <andrepadez@users.noreply.github.com>
2024-06-27 03:22:39 +0100

    back

--- workspaces/servers/admin-app/src/Layouts/Dashboard/index.tsx
+++ workspaces/servers/admin-app/src/Layouts/Dashboard/index.tsx
@@ -6,7 +6,7 @@ export const DashboardLayout = ({ children, user }) => {
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="htmx-config" content='{"selfRequestsOnly": false}' />
         <title>Document</title>
         <link rel="stylesheet" href="/tailwind.css" />
