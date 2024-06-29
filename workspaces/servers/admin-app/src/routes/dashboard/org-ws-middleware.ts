2da366a6b712f89233bbdb0cd752755889c2febb
André Alçada Padez <andrepadez@users.noreply.github.com>
2024-06-29 19:12:12 +0100

    LazyLoader

--- workspaces/servers/admin-app/src/routes/dashboard/org-ws-middleware.ts
+++ workspaces/servers/admin-app/src/routes/dashboard/org-ws-middleware.ts
@@ -1,5 +1,7 @@
 import { db, eq, Websites, Companies } from 'pertentodb';
 import { TOKENS, COMPANIES } from '@/sessions';
+const { BUILD_ENV } = process.env;
+const isProduction = BUILD_ENV === 'production';
 
 export const orgAndWebsiteMiddleware = async (c, next) => {
   const { user, token } = c.var;
@@ -59,12 +61,14 @@ export const orgAndWebsiteMiddleware = async (c, next) => {
     url.searchParams.set('org', companies[0].id);
     url.searchParams.set('ws', companies[0].websites[0]?.id);
     console.log(url.toString());
+    url.protocol = isProduction ? 'https' : 'http';
     return c.redirect(url.toString());
   } else if (!c.req.query('ws')) {
     const url = new URL(c.req.url);
     const company = companies.find((company) => company.id === +c.req.query('org'));
     url.searchParams.set('ws', company?.websites[0]?.id);
     console.log(url.toString());
+    url.protocol = isProduction ? 'https' : 'http';
     return c.redirect(url.toString());
   } else {
     const company = companies.find((company) => company.id === +c.req.query('org'));
