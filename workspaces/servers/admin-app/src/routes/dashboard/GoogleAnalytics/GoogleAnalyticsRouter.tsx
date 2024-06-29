022ed48e58a02b3aa7f8fd388aa09906f48d8007
André Alçada Padez <andrepadez@users.noreply.github.com>
2024-06-29 20:48:10 +0100

    referrer

--- workspaces/servers/admin-app/src/routes/dashboard/GoogleAnalytics/GoogleAnalyticsRouter.tsx
+++ workspaces/servers/admin-app/src/routes/dashboard/GoogleAnalytics/GoogleAnalyticsRouter.tsx
@@ -80,7 +80,7 @@ const Header = () => {
   return (
     <div class="flex items-center justify-end">
       <div class="mt-2 flex items-center gap-x-3 lg:mt-4">
-        <button class="flex w-1/2 shrink-0 items-center justify-center gap-x-2 rounded-lg bg-blue-500 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 hover:bg-blue-600 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-500">
+        <button class="flex shrink-0 items-center justify-center gap-x-2 rounded-lg bg-blue-500 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 hover:bg-blue-600 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-500">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
