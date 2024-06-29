2c6c81b7bf1f25f4812a97d9124e227b3051ca93
André Alçada Padez <andrepadez@users.noreply.github.com>
2024-06-29 20:54:31 +0100

    referrer

--- workspaces/servers/admin-app/src/routes/dashboard/_layout/DashboardLayout.tsx
+++ workspaces/servers/admin-app/src/routes/dashboard/_layout/DashboardLayout.tsx
@@ -16,7 +16,7 @@ export const DashboardLayout = ({ children, c, user, url }) => {
       </head>
       <body class="bg-[#101828]">
         <Header c={c} user={user} url={url} />
-        <main class="mx-auto min-h-[calc(100dvh-4rem)] w-full bg-white px-2 pt-20">{children}</main>
+        <main class="mx-auto min-h-[calc(100dvh-4rem)] w-full bg-white px-2 py-20">{children}</main>
         <Footer c={c} user={user} url={url} />
         <script>lucide.createIcons();</script>
       </body>
