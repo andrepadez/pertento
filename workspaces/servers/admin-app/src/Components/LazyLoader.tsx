5955804fb3927ee46876a43ea3056d484cba64ec
André Alçada Padez <andrepadez@users.noreply.github.com>
2024-06-30 02:36:44 +0100

    sorting table

--- workspaces/servers/admin-app/src/Components/LazyLoader.tsx
+++ workspaces/servers/admin-app/src/Components/LazyLoader.tsx
@@ -1,4 +1,9 @@
-export const LazyLoader = ({ children, url }) => {
+const { BUILD_ENV } = process.env;
+const isProduction = BUILD_ENV === 'production';
+
+export const LazyLoader = ({ children, ...props }) => {
+  const url = new URL(props.url);
+  url.protocol = isProduction ? 'https' : 'http';
   return (
     <div hx-target="this" hx-swap="outerHTML" hx-trigger="load" hx-get={url}>
       <h1>Loading....</h1>
