68da9b440d04efe790d9718fe63d12d13e32ec47
André Alçada Padez <andrepadez@users.noreply.github.com>
2024-07-03 07:48:32 +0100

    CRUD cookieTargeting

--- workspaces/dashboard-app/src/Dashboard/Experiments/ExperimentDetails/ExperimentSettings/ExperimentConfig/index.jsx
+++ workspaces/dashboard-app/src/Dashboard/Experiments/ExperimentDetails/ExperimentSettings/ExperimentConfig/index.jsx
@@ -4,6 +4,8 @@ import { UrlTargeting } from './UrlTargeting';
 import { DeviceTargeting } from './DeviceTargeting';
 import { CookieTargeting } from './CookieTargeting';
 import { ActivityLog } from './ActivityLog';
+import { cn } from 'helpers/cn';
+const showCookieTargeting = localStorage.getItem('PERTENTO_COOKIE_TARGETING');
 
 export const ExperimentConfig = ({ experiment }) => {
   return (
@@ -11,9 +13,9 @@ export const ExperimentConfig = ({ experiment }) => {
       <VariantsConfig experimentId={experiment.id} />
       <GoalsConfig experimentId={experiment.id} />
       <UrlTargeting experimentId={experiment.id} />
-      <div className="grid grid-cols-2 gap-4">
+      <div className={cn('grid grid-cols-1 gap-4', showCookieTargeting && 'grid-cols-2')}>
         <DeviceTargeting experimentId={experiment.id} />
-        <CookieTargeting experimentId={experiment.id} />
+        {showCookieTargeting && <CookieTargeting experimentId={experiment.id} />}
       </div>
       <ActivityLog experimentId={experiment.id} />
     </div>
