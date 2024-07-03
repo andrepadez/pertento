b6b7bc18a05741f5b8981d7e345ef095bb85dae0
André Alçada Padez <andrepadez@users.noreply.github.com>
2024-07-03 14:43:46 +0100

    better client event handling

--- workspaces/servers/admin-app/src/Components/Details.tsx
+++ workspaces/servers/admin-app/src/Components/Details.tsx
@@ -3,10 +3,11 @@ import { stringifyFunction } from 'helpers/stringify-function';
 export const Details = ({ children, ...props }) => {
   const closeDetails = stringifyFunction((ev) => {
     const details = ev.currentTarget;
+    const cancelTimeout = () => clearTimeout(timeout);
     const timeout = setTimeout(() => {
       details?.removeAttribute('open');
     }, 300);
-    details.addEventListener('mouseenter', () => clearTimeout(timeout), { once: true });
+    details.addEventListener('mouseenter', cancelTimeout, { once: true });
   });
 
   return (
