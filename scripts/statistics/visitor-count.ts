2d91c580133e5436a978377bbae2d4047409610a
André Alçada Padez <andrepadez@users.noreply.github.com>
2024-07-07 11:17:48 +0100

    visitor-counter 3

--- scripts/statistics/visitor-count.ts
+++ scripts/statistics/visitor-count.ts
@@ -2,16 +2,16 @@ import redisClient from 'redis-client';
 import { db, eq, VisitorCount } from 'pertentodb';
 
 const updateVisitorCount = async () => {
-  const keys = await redisClient.keys('PERTENTO:VISITORS:*');
+  const keys = await redisClient.keys('PERTENTO:VISITORS_BACKUP:*');
   let variantCount = 0;
   for (let key of keys) {
     const byExperiment = await redisClient.HGETALL(key);
     for (let [variantId, count] of Object.entries(byExperiment)) {
       variantCount++;
       await db.update(VisitorCount).set({ count }).where(eq(VisitorCount.variantId, +variantId));
-      await redisClient.HSET(key.replace('VISITORS', 'VISITORS_BACKUP'), variantId, count);
+      // await redisClient.HSET(key.replace('VISITORS', 'VISITORS_BACKUP'), variantId, count);
     }
-    await redisClient.DEL(key);
+    // await redisClient.DEL(key);
   }
 
   return { experiments: keys.length, variants: variantCount };
