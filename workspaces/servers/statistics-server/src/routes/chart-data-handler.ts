31097f631b669eae85878ec8c8c77f58698e3c45
André Alçada Padez <andrepadez@users.noreply.github.com>
2024-08-07 11:56:09 +0100

    no deployed variant in chartdata

--- workspaces/servers/statistics-server/src/routes/chart-data-handler.ts
+++ workspaces/servers/statistics-server/src/routes/chart-data-handler.ts
@@ -44,18 +44,17 @@ export const crunchChartData = ({ experiment, stats, goal }) => {
   const deployedVariant = dbVariants.find((v) => !!v.deployed);
 
   const firstDate = new Date(experiment.startsAt);
-  const lastDate =
-    // !!deployedVariant
-    //   ? new Date(deployedVariant.deployed):
-    !!experiment.endsAt ? new Date(experiment.endsAt) : new Date();
+  const lastDate = !!deployedVariant
+    ? new Date(deployedVariant.deployed)
+    : !!experiment.endsAt
+      ? new Date(experiment.endsAt)
+      : new Date();
 
   let currentDate = new Date(firstDate);
   currentDate.setHours(0, 0, 0, 0);
   lastDate.setDate(lastDate.getDate() + 1);
   lastDate.setHours(0, 0, 0, 0);
   const chartData = {};
-
-  console.log(experiment.id, experiment.startsAt, experiment.endsAt, lastDate);
 
   let currentDateVal = currentDate.valueOf();
   let lastDateVal = lastDate.valueOf();
@@ -65,7 +64,7 @@ export const crunchChartData = ({ experiment, stats, goal }) => {
       date: currentDateVal,
     };
     for (let variant of dbVariants) {
-      if (variant.id !== deployedVariant.id) {
+      if (variant.id !== deployedVariant?.id) {
         dataObject[variant.id] = 0;
       }
     }
@@ -80,7 +79,7 @@ export const crunchChartData = ({ experiment, stats, goal }) => {
   let currentDataObject = chartData[currentKey];
 
   for (let stat of stats) {
-    if (stat.variant_id === deployedVariant.id) continue;
+    if (stat.variant_id === deployedVariant?.id) continue;
     while (+stat.from > +currentKey) {
       if (goal === 'Revenue') {
         for (let key in currentDataObject) {
