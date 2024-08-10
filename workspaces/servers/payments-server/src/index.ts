ccecf25290e740d74a91ae4ac5bb8737d2d1c4d1
André Alçada Padez <andrepadez@users.noreply.github.com>
2024-08-10 14:30:00 +0100

    payments server

---
+++ workspaces/servers/payments-server/src/index.ts
@@ -0,0 +1,11 @@
+import { HonoServer, userMiddleware } from 'hono-server';
+import { paymentsRouter } from './routes';
+
+const { PAYMENTS_PORT: PORT } = process.env;
+
+const app = HonoServer(PORT, 'Payments Server');
+
+app.use('/', (c) => c.json({ pertentoPaymentsServer: 'v0.1.0' }));
+
+// app.use(userMiddleware);
+app.route('/', paymentsRouter);
---
+++ workspaces/servers/payments-server/src/routes/index.ts
@@ -0,0 +1,18 @@
+import { Hono } from 'hono';
+import Stripe from 'stripe';
+const { STRIPE_SECRET_KEY, STRIPE_WEBHOOK_SECRET } = process.env;
+const stripe = new Stripe(STRIPE_SECRET_KEY);
+
+export const paymentsRouter = new Hono();
+
+paymentsRouter.post('/webhook', async (ctx) => {
+  const signature = ctx.req.header('stripe-signature');
+  console.log(signature);
+  if (!signature) return ctx.text('No signature', 400);
+  const body = await ctx.req.text();
+  const event = await stripe.webhooks.constructEventAsync(body, signature, STRIPE_WEBHOOK_SECRET);
+  console.log(event.type);
+  // console.log(event.data.object);
+  console.log(Object.keys(event));
+  return ctx.json({ webhook: 'received' });
+});
