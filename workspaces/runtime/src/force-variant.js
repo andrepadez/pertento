9857a435cf7a9b001850d471ee0d30ba88f78539
André Alçada Padez <andrepadez@users.noreply.github.com>
2024-10-08 13:23:44 +0100

    runtime checkIfForceVariant 2

--- workspaces/runtime/src/force-variant.js
+++ workspaces/runtime/src/force-variant.js
@@ -1,7 +1,7 @@
 import { log } from 'helpers/injector/console';
 
 export const checkIfForceVariant = () => {
-  const url = URL(window.location.href);
+  const url = new URL(window.location.href);
   const isForceVariant = url.searchParams('pertento-force-variant');
   const isOnlyVariant = url.searchParams('pertento-only-variant');
   log('checkIfForceVariant', !isForceVariant && !isOnlyVariant, { isForceVariant, isOnlyVariant });
