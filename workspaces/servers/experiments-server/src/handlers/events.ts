1d2a3ed5577cb8a0fe453ec6ab856710f0655645
André Alçada Padez <andrepadez@users.noreply.github.com>
2024-10-12 13:38:10 +0100

    events .data

--- workspaces/servers/experiments-server/src/handlers/events.ts
+++ workspaces/servers/experiments-server/src/handlers/events.ts
@@ -25,7 +25,7 @@ export const eventsMiddleware = async (c, next) => {
   for (let event of c.req.body.dataPayload || c.req.body) {
     if (Array.isArray(event)) {
       for (let { experimentId, variantId } of experimentVariantMap) {
-        const data = JSON.stringify({ ...event[1], expVariantMap });
+        const data = JSON.stringify({ ...event[1], experimentVariantMap });
         const key = `PERTENTO:DATALAYER:${websiteId}:${experimentId}:${variantId}:${event[0]}`;
         await client.HSET(key, nowValue, data);
         const actionField = event[1]?.actionField || event[1];
